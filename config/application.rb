f25d50cf097a60a6dddddfa6aea4b7e0870b0062
Jean-Etienne MAGNE <jemag@smile.fr>
2013-11-14 14:42:02 +0100

    Set the langue to english instead of de

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ Bundler.require(:default, Rails.env)
 module Zarafadashboard
   class Application < Rails::Application
     config.time_zone = 'Central Time (US & Canada)'
-    config.i18n.default_locale = :de
+    config.i18n.default_locale = :en
 
     config.action_view.field_error_proc = Proc.new { |html_tag, instance| "<span class='field_with_errors'>#{html_tag}</span>".html_safe }
   end
