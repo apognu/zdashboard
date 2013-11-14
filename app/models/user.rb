53f92ef188bcaa27dbaf5e78428d7b7db4d07984
Antoine POPINEAU <antoine.popineau@smile.fr>
2013-11-14 14:00:04 +0100

    Forgot some madatory characters is regex for UID. :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveLdap::Base
 
   belongs_to :groups, :class => 'Group', :many => 'memberUid', :foreign_key => 'uid'
 
-  validates :uid, :presence => true, format: { with: /\A[a-zA-Z.-]+\z/, message: "only allows letters, numbers, dashes and dots" }
+  validates :uid, :presence => true, format: { with: /\A[0-9a-zA-Z._-]+\z/, message: "only allows letters, numbers, dashes and dots" }
   validates :givenName, :presence => true
   validates :surname, :presence => true
   validates :mail, :presence => true
