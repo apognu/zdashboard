bf9ca6e7b7e0b3b08a8a24572e958ef932c223c6
Antoine POPINEAU <antoine.popineau@smile.fr>
2013-11-15 12:02:08 +0100

    Errors are now displayed on the login page if no credentials were given.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   before_filter :authorize
 
+  def initialize
+    @messages = Hash.new
+    super
+  end
+
   def authorize
     begin
       if session[:user]
@@ -13,29 +18,34 @@ class ApplicationController < ActionController::Base
 
       auth
     rescue ActiveLdap::ConnectionError
-      @messages ||= Hash.new
       @messages[:danger] = 'We could not connect to the backend direcotry, try again later.' 
       render 'application/auth' and return
     end
   end
 
   def auth
-    if ! params[:username].nil? and ! params[:password].nil?
-      user = User.find(params[:username])
+    begin
+      raise RuntimeError, 'Both fields are mandatory, here.' if params.has_key?(:username) and
+                                                                (params[:username].empty? or params[:password].empty?)
 
-      if '{sha256}' << Digest::SHA2.new.base64digest(params[:password]) == user.userPassword
-        if user.zarafaAdmin
-          session[:user] = user.uid
+      if ! params[:username].nil? and ! params[:password].nil?
+        user = User.find(params[:username])
 
-          redirect_to(session[:return_to]) and return
-          return
+        if '{sha256}' << Digest::SHA2.new.base64digest(params[:password]) == user.userPassword
+          if user.zarafaAdmin
+            session[:user] = user.uid
+
+            redirect_to(session[:return_to]) and return
+            return
+          end
         end
       end
+
+      session[:return_to] = request.original_url
+      render 'application/auth' and return
+    rescue RuntimeError => error
+      @messages[:danger] = error
     end
-
-    session[:return_to] = request.original_url
-
-    render 'application/auth' and return
   end
 
   def logout
